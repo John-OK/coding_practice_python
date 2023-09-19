b76b833b8d511d0ce705dbb7ae73382a5eb38e19
John O'Keeffe <john.ok.dev@gmail.com>
2023-09-19 17:16:02 +0200

    Add alternate solution to running_sum_of_1d_array. Solved 2023-09-19.

--- leetcode_1480_running_sum_of_1d_array.py
+++ leetcode_1480_running_sum_of_1d_array.py
@@ -1,10 +1,18 @@
 def runningSum(nums: list[int]) -> list[int]:
-    sum_list: list[int] = []
-    for index, num in enumerate(nums):
-        if sum_list:
-            sum_list.append(num + sum_list[index-1])
-        else:
-            sum_list.append(num)
-    return sum_list
+    # sum_list: list[int] = []
+    # for index, num in enumerate(nums):
+    #     if sum_list:
+    #         sum_list.append(num + sum_list[index-1])
+    #     else:
+    #         sum_list.append(num)
+    # return sum_list
 
-print(runningSum([1, 2, 3, 4]))
+
+    # 2nd Attempt: basically the same algorithm but modifies the original list to save space. Avoid when possible as modifying the inputs can have unexpected side effects.
+    for i in range(1, len(nums)):
+        nums[i] += nums[i-1]
+    return nums
+
+print(runningSum([1, 2, 3, 4])) # => [1,3,6,10]
+print(runningSum([1,1,1,1,1])) # => [1,2,3,4,5]
+print(runningSum([3,1,2,10,1])) # => [3,4,6,16,17]
