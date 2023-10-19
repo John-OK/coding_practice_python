12f652cdba17b50261be217f96135aabc8b3f7d7
John O'Keeffe <john.ok.dev@gmail.com>
2023-10-19 20:44:33 +0200

    Fixed uncaught error in valid_anagram solution, and added one-liner soltion.

--- leetcode_242_valid_anagram.py
+++ leetcode_242_valid_anagram.py
@@ -1,21 +1,17 @@
+from collections import Counter
 def isAnagram(s: str, t: str) -> bool:
-    # FIRST ATTEMPT: time complexity: O(n)
-    letter_count = {}
+    # FIRST ATTEMPT: time complexity: O(n), 51 ms, beats 72.75%
+    # Could do in one line: return Counter(s) == Counter(t)
+    count_s, count_t = Counter(s), Counter(t)
 
     if len(s) != len(t):
         return False
 
-    for letter in s:
-        if letter in letter_count:
-            letter_count[letter] += 1
-        else:
-            letter_count[letter] = 1
-
-    for letter in letter_count:
-        if t.count(letter) != letter_count[letter]:
+    for letter in count_s:
+        if count_s[letter] != count_t[letter]:
             return False
 
     return True
 
-print(isAnagram('anagram', 'nagaram'))
-print(isAnagram('rat', 'car'))
+print(isAnagram('anagram', 'nagaram')) # => True
+print(isAnagram('rat', 'car')) # => False
