fcbf2927c3d40f9202f518ddce2c78e7d32c37f3
John O'Keeffe <john.ok.dev@gmail.com>
2023-10-26 20:25:51 +0200

    Added alternate solution that could be faster in some situation, but could also be around n**2.

--- leetcode_49_group_anagrams.py
+++ leetcode_49_group_anagrams.py
@@ -21,6 +21,23 @@ def groupAnagrams(strs: list[str]) -> list[list[str]]:
 
     return [v for k, v in hashTable.items()]
 
+
+    #SECOND ATTEMPT: time complexity: O(n * m) where m = number of input strings, and n = avg length of those strings; strings, 101 ms, beats 52.89%
+    # from collections import defaultdict
+
+    # char_freqs = defaultdict(list) # defaultdict handles cases where key:value does not yet exist in dict 
+
+    # for word in strs: # "m" in time complexity
+    #     count = [0] * 26 # list of count of chars a to z (all initially set to 0)
+
+    #     for char in word: "n" in time complexity
+    #         count[ord(char) - ord("a")] += 1 # ord() gets the ASCII value of char; subtract by ord("a") to get index of char ("a" - "a" = 0; "b" - "a" = 1; ...; "z" - "a" = 25)
+
+    #     char_freqs[tuple(count)].append(word) # tuple becuase keys must be immutable
+
+    # return char_freqs.values()
+
+
 print(groupAnagrams(["eat","tea","tan","ate","nat","bat"])) # => [["bat"],["nat","tan"],["ate","eat","tea"]]
 print(groupAnagrams([""])) # => [[""]]
 print(groupAnagrams(["a"])) # => [["a"]]
