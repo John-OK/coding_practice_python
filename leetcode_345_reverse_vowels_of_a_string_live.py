b2bb6456a2ca130f3f660278331e62df411be83e
John O'Keeffe <john.ok.dev@gmail.com>
2023-11-08 19:32:18 +0100

    Add optimized solution to reverse_vowels_of_a_string. Solved 2023-11-08.

--- leetcode_345_reverse_vowels_of_a_string_live.py
+++ leetcode_345_reverse_vowels_of_a_string_live.py
@@ -28,26 +28,44 @@ def reverseVowels(s: str) -> str:
 
 
     # SECOND ATTEMPT (simplified code, but worse time complexity): time complexity: O(n), 204 ms, beats 5.01%
+    # vowels = "aeiouAEIOU"
+    # vowel_indices = []
+    # vowel_chars_reversed = []
+    # reversed_vowels = list(s)
+    
+    # for i, char in enumerate(s):
+    #     if char in vowels:
+    #         vowel_indices.append(i)
+    #         vowel_chars_reversed.insert(0, char)
+
+    # for i, vowel_index in enumerate(vowel_indices):
+    #     reversed_vowels[vowel_index] = vowel_chars_reversed[i]
+
+    # return "".join(reversed_vowels)
+    
+    
+
+    # THIRD ATTEMPT (using 2 pointers): time complexity: O(n), 42 ms, beats 97.49%
     vowels = "aeiouAEIOU"
-    vowel_indices = []
-    vowel_chars_reversed = []
-    reversed_vowels = list(s)
+    s_list = list(s)
+    left = 0
+    right = len(s) - 1
+
+    while left < right:
+        if s_list[left] not in vowels:
+            left += 1
+        else:
+            if s_list[right] not in vowels:
+                right -= 1
+            else:
+                right_char = s_list[right]
+                s_list[right] = s_list[left]
+                s_list[left] = right_char
+                
+                left += 1
+                right -= 1
     
-    for i, char in enumerate(s):
-        if char in vowels:
-            vowel_indices.append(i)
-            vowel_chars_reversed.insert(0, char)
-
-    for i, vowel_index in enumerate(vowel_indices):
-        reversed_vowels[vowel_index] = vowel_chars_reversed[i]
-
-    return "".join(reversed_vowels)
-    
-    
-
-# THIRD ATTEMPT (using 2 pointers): time complexity: O(n), 45 ms, beats 94.40%
-
-
+    return "".join(s_list)
 
 print(reverseVowels("hello")) # => "holle"
 print(reverseVowels("leetcode")) # => "leotcede"
