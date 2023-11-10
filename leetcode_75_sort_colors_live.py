f8e8781b2ad2112c646c1c0de5788aad0e510c8d
John O'Keeffe <john.ok.dev@gmail.com>
2023-11-10 12:59:30 +0100

    Add solution with better time complexity. Solved 2023-11-10.

--- leetcode_75_sort_colors_live.py
+++ leetcode_75_sort_colors_live.py
@@ -1,22 +1,39 @@
 def sortColors(nums: list[int]) -> None:
     # FIRST ATTEMPT: time complexity: O(n**2), 45 ms, beats 44.10%
-    left = 0
-    right = 1
-    have_switched = True
+    # left = 0
+    # right = 1
+    # have_switched = True
 
-    while have_switched:
-        have_switched = False
-        left = 0
-        right = 1
-        while right < len(nums):
-            if nums[left] > nums[right]:
-                nums[right], nums[left] = nums[left], nums[right]
-                left += 1
-                right += 1
-                have_switched = True
-            else:
-                left += 1
-                right += 1
+    # while have_switched:
+    #     have_switched = False
+    #     left = 0
+    #     right = 1
+    #     while right < len(nums):
+    #         if nums[left] > nums[right]:
+    #             nums[right], nums[left] = nums[left], nums[right]
+    #             left += 1
+    #             right += 1
+    #             have_switched = True
+    #         else:
+    #             left += 1
+    #             right += 1
+
+
+
+    SECOND ATTEMPT (hashmap): time complexity: O(n), 31 ms, beats 97.19%
+    from collections import defaultdict
+    freq_map = defaultdict(list)
+
+    for num in nums:
+        freq_map[num].append(num)
+
+    i = 0
+    for num in range(3):
+        freq = freq_map[num]
+        nums[i:i + len(freq)] = freq_map[num]
+        i += len(freq)
+
+    return nums
 
 print(sortColors([2,0,2,1,1,0])) # => [0, 0, 1, 1, 2, 2]
 print(sortColors([2,0,1])) # => [0, 1, 2]
